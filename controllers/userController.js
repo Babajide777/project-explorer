4a274aff8260c2c681f1be6d3100a9b832fb7e08
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2022-02-14 21:05:39 -0600

    get by email function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,8 @@
-const { authenticateUser } = require("../services/userService");
+const {
+  authenticateUser,
+  createUser,
+  getByEmail,
+} = require("../services/userService");
 const { userRegisterValidation } = require("../services/validation");
 const { responseHandler } = require("../utils/responseHandler");
 
@@ -28,6 +32,12 @@ const userSignup = async (req, res) => {
     let allErrors = details.map((detail) => detail.message.replace(/"/g, ""));
     return responseHandler(res, allErrors, 400, false, "");
   }
+  const anyUsername = await getByEmail(req.body.email);
+
+  if (anyUsername) {
+    return responseHandler(res, "Username not available", 400, false, "");
+  }
+  const check = await createUser(req.body);
 };
 
 module.exports = { userLogin, userSignup };
--- services/userService.js
+++ services/userService.js
@@ -1,31 +1,6 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 
-// Authenticate a user
-const authenticateUser = async (email, password) => {
-  const user = new User();
-  user.email = email;
-  user.password = password;
-  const result = await getByEmail(email);
-
-  return result && user.validPassword(result, user.password)
-    ? [true, result]
-    : [false, ["Invalid email/password"]];
-};
-
-// Return user with specified email
-// const getByEmail = async (email) => {
-//   const user = await User.findOne({ email: email });
-//   if (user) {
-//     user.profilePicture = scaledPicture(user.profilePicture);
-//   }
-//   return user;
-// };
-
-// const scaledPicture = (pic) => {
-//   return pic.replace("/upload", "/upload/c_scale,h_50,q_auto:best,w_50");
-// };
-
 //To hash pasword
 const hashedPassword = async (password) => {
   const salt = await bcrypt.genSalt(15);
@@ -43,14 +18,15 @@ const createUser = async ({
   graduationYear,
 }) => {
   try {
-    let newUser = new User();
-    newUser.firstName = firstName;
-    newUser.lastName = lastName;
-    newUser.email = email;
-    newUser.password = await hashedPassword(password);
-    newUser.program = program;
-    newUser.matricNumber = matricNumber;
-    newUser.graduationYear = graduationYear;
+    let newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: await hashedPassword(password),
+      program,
+      matricNumber,
+      graduationYear,
+    });
     if (await newUser.save()) {
       return [true, signJwt(newUser), newUser];
     }
@@ -58,6 +34,32 @@ const createUser = async ({
     return [false, translateError(err)];
   }
 };
+
+// Authenticate a user
+const authenticateUser = async (email, password) => {
+  const user = new User();
+  user.email = email;
+  user.password = password;
+  const result = await getByEmail(email);
+
+  return result && user.validPassword(result, user.password)
+    ? [true, result]
+    : [false, ["Invalid email/password"]];
+};
+
+// Return user with specified email
+const getByEmail = async (email) => await User.findOne({ email });
+
+// if (user) {
+//     user.profilePicture = scaledPicture(user.profilePicture);
+//   }
+
+// const scaledPicture = (pic) => {
+//   return pic.replace("/upload", "/upload/c_scale,h_50,q_auto:best,w_50");
+// };
+
 module.exports = {
   authenticateUser,
+  createUser,
+  getByEmail,
 };
